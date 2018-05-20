0e36aa23ada1e62b03e87ea976dc77a363ca6489
Rustom Miguel Nava <rmnavz@icloud.com>
2018-05-20 08:10:59 +0800

    Web Project Creation

---
+++ ASPNETCore_WebApp/Data/DbInitializer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCore_WebApp.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(DatabaseContext context)
+        {
+            context.Database.EnsureCreated();
+
+            ////Look for any Pages.
+            //if (context.Sys_Pages.Any())
+            //{
+            //    return; //DB has been seeded
+            //}
+
+            ////Setup your pages here
+            //var Sys_Pages = new Sys_PageModel[]
+            //{
+            //    new Sys_PageModel
+            //    {
+            //        PageTitle = "Home",
+            //        PageDescription = "The home page of the website",
+            //        PageController = "Home",
+            //        PageAction = "Index",
+            //        IsPublic = true,
+            //        DateCreated = DateTime.Now,
+            //        IsEnabled = true
+            //    }
+            //};
+
+            ////Add Pages to the system page database
+            //foreach (Sys_PageModel page in Sys_Pages)
+            //{
+            //    context.Sys_Pages.Add(page);
+            //}
+            //context.SaveChanges();
+        }
+    }
+}
---
+++ ASPNETCore_WebApp/Startup.cs
@@ -0,0 +1,77 @@
+﻿using ASPNETCore_WebApp.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ASPNETCore_WebApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DatabaseContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("LiveConnection")));
+
+            services.AddMvc(options =>
+            {
+                options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
+                //options.Filters.Add(new RequireHttpsAttribute()); //Enable this to require HTTPS connection to your Web App
+            });
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.AccessDeniedPath = "/Error/Forbidden";
+                    options.LoginPath = "/Account/Index";
+                });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("MustBeAdmin", p => p.RequireAuthenticatedUser().RequireRole("admin"));
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
